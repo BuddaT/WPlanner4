e9a4d1d367389045429b8773aec4344e7162b6cf
Blake Tily <budda13@gmail.com>
2013-10-31 19:40:13 +1000

    Main window initialising, close if multiple instance

--- src/main/java/net/buddat/wplanner/WPlanner.java
+++ src/main/java/net/buddat/wplanner/WPlanner.java
@@ -1,22 +1,52 @@
 package net.buddat.wplanner;
 
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import javax.swing.JOptionPane;
+
 import appinstance.AppLock;
 
-/**
- * Hello world!
- *
- */
-public class WPlanner 
-{
-    public static void main( String[] args )
-    {
-        AppLock.setLock("WPlanner");
-        System.out.println( "Hello World!" );
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+public class WPlanner extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO: Start polling argument file from multiple instances.
+
+		Group rootGroup = new Group();
+		Scene rootScene = new Scene(rootGroup, 800, 600);
+
+		primaryStage.setScene(rootScene);
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+			@Override
+			public void handle(WindowEvent w) {
+				// TODO: Prompt to save, clean up etc.
+				System.exit(0);
+			}
+			
+		});
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		if (!AppLock.setLock("WPlanner")) {
+			// Another instance is already running.
+			// TODO: Pass any arguments to first instance via argument file.
+			if (args.length > 0) {
+
+			} else {
+				JOptionPane.showMessageDialog(null,
+						"Application is already running. Exiting.");
+				System.exit(0);
+			}
+		} else {
+			launch();
         }
     }
+
 }
