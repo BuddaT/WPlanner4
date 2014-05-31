779a23787353c2e0e7041ae9b698dea883b48827
BuddaT <budda13@gmail.com>
2014-05-31 18:17:57 +0800

    Basic pattern for creating toolbar items. Also testing commit & push.

--- src/main/java/net/buddat/wplanner/WPlanner.java
+++ src/main/java/net/buddat/wplanner/WPlanner.java
@@ -1,5 +1,7 @@
 package net.buddat.wplanner;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,11 +13,16 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
 import net.buddat.wplanner.settings.AppSettings;
@@ -71,25 +78,61 @@ public class WPlanner extends Application {
 
 	private void setPrimaryScene(Scene s) {
 		MenuBar menuBar = new MenuBar();
+		{
+			Menu menuFile = new Menu("File");
+			Menu menuEdit = new Menu("Edit");
+			Menu menuView = new Menu("View");
 
-		Menu menuFile = new Menu("File");
-		Menu menuEdit = new Menu("Edit");
-		Menu menuView = new Menu("View");
+			MenuItem quit = new MenuItem("Exit");
+			quit.setAccelerator(KeyCombination.keyCombination("ALT+F4"));
+			quit.setOnAction(new EventHandler<ActionEvent>() {
 
-		MenuItem quit = new MenuItem("Exit");
-		quit.setAccelerator(KeyCombination.keyCombination("ALT+F4"));
-		quit.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent t) {
+					close();
+				}
 
-			@Override
-			public void handle(ActionEvent t) {
-				close();
+			});
+			menuFile.getItems().addAll(new SeparatorMenuItem(), quit);
+
+			menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+		}
+		
+		ToolBar toolBar = new ToolBar();
+		{
+			try {
+				BufferedImage finalImg = ImageIO.read(getClass()
+						.getResourceAsStream("/data/gui/pointer.png"));
+				java.awt.Image toUse = finalImg.getScaledInstance(16, 16, BufferedImage.SCALE_SMOOTH);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
+			ImageView testView = new ImageView(new Image(
+					"/data/gui/pointer.png", 24, 24, true, true));
 
-		});
-		menuFile.getItems().addAll(new SeparatorMenuItem(), quit);
+			ToggleButton testButton = new ToggleButton(null, testView);
+			testButton.setOnAction(new EventHandler<ActionEvent>() {
 
-		menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
-		((VBox) s.getRoot()).getChildren().addAll(menuBar);
+				@Override
+				public void handle(ActionEvent arg0) {
+					close();
+				}
+
+			});
+
+			testButton
+					.setStyle("-fx-padding: 0 0 0 0; -fx-outer-border: white;"
+							+ " -fx-background-color: -fx-outer-border, -fx-body-color;"
+							+ " -fx-background-insets: 0, 0; -fx-background-radius: 0px, 0px;");
+			testButton.setScaleX(0.75);
+			testButton.setScaleY(0.75);
+			toolBar.setStyle("-fx-padding: 0 0 0 0; -fx-background-color: -fx-outer-border, -fx-body-color;");
+
+			toolBar.getItems().add(testButton);
+			toolBar.autosize();
+		}
+
+		((VBox) s.getRoot()).getChildren().addAll(menuBar, toolBar);
 	}
 
 	private void close() {
